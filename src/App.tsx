20d59c3a5f7d7a5e7f548cf1c8781c8db1cc360c
miguel.roldan <miguel.roldan@pragma.com.co>
2025-06-12 18:40:11 -0500

    feature: useReducer added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import MemoComp from "./pages/useMemo/useMemo";
 import CallbackComp from "./pages/useCallback";
 import UseContext from "./pages/useContext";
 import UseRefComp from "./pages/useRef/useRefComp";
+import UseReducerComp from "./pages/useReducer/UseReducerComp";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/use-callback" element={<CallbackComp />} />
         <Route path="/use-context" element={<UseContext />} />
         <Route path="/use-ref" element={<UseRefComp />} />
+        <Route path="/use-reducer" element={<UseReducerComp />} />
       </Routes>
     </div>
   );
